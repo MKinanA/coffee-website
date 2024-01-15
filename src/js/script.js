b0f6ea27d33b3342b6a2b96d4f76a126e4698f58
MKinanA <mkinanahsan@gmail.com>
2024-01-15 13:57:54 +0700

    update 15012024

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 // navbar fixed
 const header = document.querySelector('header'),
-navbar_fixed = header.offsetTop;
+navbar_fixed = header.offsetTop,
+nav_links = document.querySelector('nav#navmenu');
 window.onscroll = function() {
     if (window.scrollY > navbar_fixed) {
         header.classList.add('navbar-fixed');
@@ -8,3 +9,10 @@ window.onscroll = function() {
         header.classList.remove('navbar-fixed');
     };
 };
+
+// hamborgir
+const hamborgir = document.getElementById('hamborgir');
+hamborgir.addEventListener('click', function() {
+    hamborgir.classList.toggle('active');
+    nav_links.classList.toggle('hidden');
+});
